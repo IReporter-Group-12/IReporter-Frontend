f7e5e837c3bd9448d34f09c6895f010d453e393a
edith <edith.chelangat@student.moringaschool.com>
2024-05-21 18:00:57 +0300

    add file and add cards

---
+++ src/components/AdminDashbord.js
@@ -0,0 +1,18 @@
+// AdminDashboard.js
+import React from 'react';
+import "../styles/AdminDashboard.css"
+
+
+// Define the AdminDashboard component
+function AdminDashboard() {
+  return (
+    <div className="dashboard-container">
+      <div className="card">Red Flag</div>
+      <div className="card">Red Flag</div>
+      <div className="card">Red Flag</div>
+      {/* Add more cards as needed */}
+    </div>
+  );
+}
+
+export default AdminDashboard;
